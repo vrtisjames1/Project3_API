73f3a9750765e4618937dd2ffca2c6c444fe6695
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-12-09 11:00:20 -0500

    updated status

--- models/students.js
+++ models/students.js
@@ -4,7 +4,7 @@ const studentSchema = new mongoose.Schema({
     parent: String,
     kid: String,
     photo: String,
-    update: [{date: String, header: String, comments: String}],
+    status: [{date: String, header: String, comments: String}],
 });
 
 const Students = mongoose.model('Students', studentSchema);
