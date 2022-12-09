837f711c272f210c1e6f5d164f6ade8f028f0bee
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-12-09 09:07:11 -0500

    update

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 // const Travel = require('./models/travel.js');
 const app = express();
 
+let PORT = 3000;
+if(process.env.PORT){
+	PORT = process.env.PORT
+}
+
 //creates rect.body off of url encoded data
 app.use(express.json()); //use .json(), not .urlencoded()
 app.use(cors()); //allow everyone to access your data
@@ -12,6 +17,12 @@ app.use(cors()); //allow everyone to access your data
 // =======================================
 // const appRouter = require("./controllers/routes.js");
 
+//test route
+// app.get('/', (req, res)=>{
+//         res.send('hello');
+// });
+
+
 // app.use("/travel", appRouter);
 
 ////drop database comment
@@ -19,11 +30,11 @@ app.use(cors()); //allow everyone to access your data
 
 //=================================================
 // connections
-mongoose.connect('mongodb://localhost:27017/travelcrud')
+mongoose.connect('mongodb://localhost:27017/proj3')
 mongoose.connection.once('open', ()=>{
     console.log('connected to mongod...');
 });
 
-app.listen(3000, ()=>{
-    console.log('travel listening...');
-});
+app.listen(PORT, ()=>{
+	console.log('listening');
+})
