d7a5d91d40dc675a54b707837a6715ef969b9d3b
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-12-12 20:11:41 -0500

    update

--- controllers/routes.js
+++ controllers/routes.js
@@ -58,7 +58,9 @@ router.delete('/:id', (req, res)=>{
 });
 
 //create update route
+//confirm with changes tommorrow
 router.put('/:id', (req, res)=>{
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     Students.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedStudents)=>{
         res.json(updatedStudents);
     });
